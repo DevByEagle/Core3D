68816b9c026b27aacfcf2ad7637705980514f2cc
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-07-26 20:50:47 +0000

    Refactor CMake configuration: improve organization and comments for OpenGL backend linking

--- cmake/LibraryConfigurations.cmake
+++ cmake/LibraryConfigurations.cmake
@@ -1,23 +1,29 @@
 if(CORE3D_USE_OPENGL)
-    # GLEW and OpenGL Linking
     if(APPLE)
-        list(REMOVE_ITEM CORE3D_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/Backends/OpenGL/GLBackend.cpp")
-        list(REMOVE_ITEM CORE3D_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/Backends/OpenGL/GLBackend.h")
+        list(REMOVE_ITEM CORE3D_SOURCES
+            "${CMAKE_CURRENT_SOURCE_DIR}/Backends/OpenGL/GLBackend.cpp"
+            "${CMAKE_CURRENT_SOURCE_DIR}/Backends/OpenGL/GLBackend.h"
+        )
         message(FATAL_ERROR "OpenGL backend is not supported on macOS. Please use Metal instead.")
+
     elseif(WIN32)
+        # GLEW
         add_subdirectory(${CMAKE_SOURCE_DIR}/thirdparty/glew ${CMAKE_BINARY_DIR}/thirdparty/glew)
         target_link_libraries(Core3D PRIVATE opengl32 GLEW)
-    elseif(UNIX)
-        find_package(OpenGL REQUIRED)
-        add_subdirectory(${CMAKE_SOURCE_DIR}/thirdparty/glew ${CMAKE_BINARY_DIR}/thirdparty/glew)
-        target_link_libraries(Core3D PRIVATE OpenGL::GL GLEW)
-    endif()
 
-    # GLFW Linking
-    if(WIN32)
+        # GLFW
         add_subdirectory(${CMAKE_SOURCE_DIR}/thirdparty/glfw ${CMAKE_BINARY_DIR}/thirdparty/glfw)
         target_link_libraries(Core3D PRIVATE glfw)
-    elseif(UNIX)
+    elseif(UNIX AND NOT APPLE)
+        # OpenGL
+        find_package(OpenGL REQUIRED)
+        target_link_libraries(Core3D PRIVATE OpenGL::GL)
+
+        # GLEW
+        add_subdirectory(${CMAKE_SOURCE_DIR}/thirdparty/glew ${CMAKE_BINARY_DIR}/thirdparty/glew)
+        target_link_libraries(Core3D PRIVATE GLEW)
+
+        # GLFW
         find_package(glfw3 REQUIRED)
         target_link_libraries(Core3D PRIVATE glfw)
     endif()
