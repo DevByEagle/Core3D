d5493660ae17f6c6adfd62078049674e39bf9bbc
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-07-26 22:39:44 +0000

    Fix graphics context initialization: update preprocessor directives for OpenGL backend

--- src/GraphicsContext.cpp
+++ src/GraphicsContext.cpp
@@ -2,7 +2,7 @@
 #include "Core3D/GraphicsContext.h"
 
 // Include the appropriate backend header based on the selected graphics backend
-#if CORE3D_USE_OPENGL
+#if CORE3D_BACKEND_OPENGL
     #include "Backends/OpenGL/GLBackend.h"
 #endif
 
@@ -10,8 +10,36 @@ using namespace Core3D;
 
 GraphicsContext::GraphicsContext() 
 {
-#if CORE3D_USE_OPENGL
+#if CORE3D_BACKEND_OPENGL
     m_CurrentBackend = "OpenGL";
     GLBackend::Initialize();
+#endif
+}
+
+GraphicsContext::~GraphicsContext()
+{
+#if CORE3D_BACKEND_OPENGL
+    GLBackend::Shutdown();
+#endif
+}
+
+void GraphicsContext::Draw()
+{
+    return;
+}
+
+int GraphicsContext::GetWidth() const 
+{
+    return 0;
+}
+
+int GraphicsContext::GetHeight() const
+{
+    return 0;
+}
+
+void GraphicsContext::Present()
+{
+#if CORE3D_BACKEND_OPENGL
 #endif
 }
